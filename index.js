ebb0c03afc1cc7dad25ac2c3cf094dc0d64bfa07
Akash <akashdeep.lpu@outlook.com>
2023-01-05 13:21:04 +0530

    completed

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+
+const app = express();
+
+const http = require("http");
+
+const cors = require("cors");
+
+const {Server} = require("socket.io");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+let users = {};
+let rooms = {};
+
+
+app.get("/", (req, res, next) => {
+  res.send("<html><h1>Server is working fine...</h1></html>")
+})
+
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("user_joined", (userData) => {
+    users[socket.id] = userData.username;
+    rooms[socket.id] = userData.roomId;
+
+    socket.to(userData.roomId).emit("new_user_joined", userData);
+  });
+
+  socket.on("join_room", ({ username, roomId }) => {
+    socket.join(roomId);
+  });
+
+  socket.on("send_message", (message) => {
+    socket.to(message.roomId).emit("receive_message", {
+      message: message,
+    });
+  });
+
+  socket.on("disconnect", (message) => {
+    socket.to(rooms[socket.id]).emit("left", { username: users[socket.id] });
+    delete users[socket.id];
+  });
+});
+
+server.listen(process.env.PORT || 2000);
